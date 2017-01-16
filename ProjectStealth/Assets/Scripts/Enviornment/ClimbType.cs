12e2c4c004f9a61739eac4baf16e51c561ce85bc
Jay Lee <smugalamunga@gmail.com>
2017-01-16 10:26:19 -0500

    added comments so that I don't forget what these things do

--- ProjectStealth/Assets/Scripts/Enviornment/ClimbType.cs
+++ ProjectStealth/Assets/Scripts/Enviornment/ClimbType.cs
@@ -20,15 +20,17 @@ public class ClimbType : MonoBehaviour
     {
         get { return wall_climb; }
     }
+    // This is to know when the player should latch onto the ceiling to crawl around on
     public bool CeilingClimb
     {
         get { return ceiling_climb; }
     }
+    // This is used for down+jumping through floors
+    // To jump up through passable floors CollisionMasks.UpwardsCollisionMask
     public bool CeilingPass
     {
         get { return ceiling_pass; }
     }
-
     public bool LeftConnect
     {
         get { return left_connect; }
