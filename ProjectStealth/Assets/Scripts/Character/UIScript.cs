7622af1e4f3c75f8a7e49256cd9fdedaa6a09240
GabeV <gviolette0@gmail.com>
2017-11-07 17:57:14 -0600

    Create placeholder UI

---
+++ ProjectStealth/Assets/Scripts/Character/UIScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Player data tracking UI script
+public class UIScript : MonoBehaviour
+{
+    #region vars
+    private Player player;
+
+    public GameObject shieldUI;
+    public GameObject energyUI;
+    public GameObject cloakIcon;
+    public GameObject evadeIcon;
+
+    public bool hideAll;
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        player = GameObject.Find("PlayerCharacter").GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if ( player.IsCloaking() )
+        {
+            //cloakIcon.GetComponent<Text>();
+        }
+	}
+}
