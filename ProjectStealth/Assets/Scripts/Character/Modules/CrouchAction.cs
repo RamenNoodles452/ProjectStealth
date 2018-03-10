bab802052dd5527e1205acf79e78a74f0d1e9029
Jay Lee <smugalamunga@gmail.com>
2018-03-10 13:36:01 -0500

    oh shit

---
+++ ProjectStealth/Assets/Scripts/Character/Modules/CrouchAction.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CrouchAction : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// CharacterStats Vars:
+    /// IsCrouching
+    /// </summary>
+
+    private CharacterStats charStats;
+    private IInputManager inputManager;
+
+    void Start()
+    {
+        charStats = GetComponent<CharacterStats>();
+        inputManager = GetComponent<IInputManager>();
+    }
+
+    void Update()
+    {
+        if (charStats.OnTheGround && charStats.CurrentMoveState == CharEnums.MoveState.isSneaking && inputManager.VerticalAxis < 0f)
+        {
+            if (charStats.IsCrouching == false)
+            {
+                charStats.IsCrouching = true;
+                charStats.CrouchingHitBox();
+            }
+        }
+        else
+        {
+            if (charStats.IsCrouching)
+            {
+                charStats.IsCrouching = false;
+                charStats.StandingHitBox();
+            }
+        }
+    }
+}
