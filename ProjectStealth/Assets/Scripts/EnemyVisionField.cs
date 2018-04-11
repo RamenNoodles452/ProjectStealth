4132e214692b903ce1081b13f9a514b89b190f4f
GabeV <gviolette0@gmail.com>
2018-04-11 06:56:14 -0500

    Basic enemy sight.

--- ProjectStealth/Assets/Scripts/EnemyVisionField.cs
+++ ProjectStealth/Assets/Scripts/EnemyVisionField.cs
@@ -33,24 +33,26 @@ public class EnemyVisionField : MonoBehaviour
 
         // confirm seen by raycasting against player + wall colliders, ignoring triggers...
         // for all collisions, check if distance < distance to player
-        // what is player mask?
-        // what is wall mask?
         Vector3 playerPosition = Referencer.Instance.player.gameObject.transform.position;
         Vector3 enemyPosition = this.transform.parent.gameObject.transform.position;
         Vector2 origin = new Vector2( enemyPosition.x, enemyPosition.y );
         Vector2 direction = new Vector2( playerPosition.x - enemyPosition.x, playerPosition.y - enemyPosition.y );
         float distanceToPlayer = Mathf.Sqrt( Mathf.Pow( playerPosition.x - enemyPosition.x, 2) + Mathf.Pow( playerPosition.y - enemyPosition.y, 2) );
-        LayerMask layerMask = new LayerMask();
+		int layerMask = LayerMask.GetMask( "geometry" );
+        RaycastHit2D hit; // array?
+		hit = Physics2D.Raycast( enemyPosition, direction, distanceToPlayer, layerMask );
+		// TO CONSIDER:
+		// Overkill? if center to center raycast fails, try center to top/bottom right/left of player hitbox (allows game to have enemies see feet / heads poking out)
+		//Physics2D.RaycastAll( origin, direction, distanceToPlayer + 1.0f, layerMask );
 
-        //CollisionMasks.CharacterMask;
-
-        //RaycastHit2D hit; // array?
-        //Physics2D.RaycastAll( origin, direction, distanceToPlayer + 1.0f, layerMask );
-
-        // Overkill? if center to center raycast fails, try center to top/bottom right/left of player hitbox (allows game to have enemies see feet / heads poking out)
-
-        // if player, send "seen" message to enemy
-        //enemy.DoSomething();
+		if ( hit.collider == null )
+		{
+			// no walls between the player and the enemy, YOU WERE SEEN!
+			// if player, send "seen" message to enemy
+			//enemy.DoSomething();
+			Debug.Log( "You were seen!" );
+			GameState.Instance.IsRedAlert = true; // testing.
+		}
     }
 
     private void OnTriggerStay2D( Collider2D collision )
