8aabe3bb5c8781e8948eeee5e72fdd79a49a5bb1
GabeV <gviolette0@gmail.com>
2018-10-25 04:58:34 -0400

    Create grappling hook.

---
+++ ProjectStealth/Assets/Scripts/Character/GrapplingHook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Grappling hookshot
+public class GrapplingHook : MonoBehaviour
+{
+    #region vars
+    private const float MAX_RANGE = 100.0f; // pixels
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        //
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
+    
+    // Launch the hookshot!
+    void Fire()
+    {
+        // raycast, check if object is hookshottable (pass through pass through platforms)
+        // alt: check if enemy is hookshottable
+        // Not hookshottable: Make a Dink! noise & spark.
+    }
+}
