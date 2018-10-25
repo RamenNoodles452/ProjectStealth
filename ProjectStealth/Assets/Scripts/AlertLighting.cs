d82f976f031a4df7113cb44441c2e310ccfb0477
GabeV <gviolette0@gmail.com>
2018-10-25 05:22:22 -0400

    Subdue red alert overlay. Add pulse to alert lighting.

--- ProjectStealth/Assets/Scripts/AlertLighting.cs
+++ ProjectStealth/Assets/Scripts/AlertLighting.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// Changes the color of the light to RED when on alert.
+// Changes the color of the light to RED, and makes it pulse when on alert.
 public class AlertLighting : MonoBehaviour
 {
     #region vars
+    [SerializeField]
+    private float period = 1.0f; // time to do a full pulse, in seconds
+    [SerializeField]
+    [Range (0.0f, 1.0f)]
+    private float  minimum_brightness = 0.10f; // as a % of maximum brightness
+
     private Light light;
     private Color safe_color;
     private float intensity;
@@ -13,27 +19,27 @@ public class AlertLighting : MonoBehaviour
     #endregion
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         light = GetComponent<Light>();
         safe_color = light.color;
         intensity = light.intensity;
     }
-    
+
     // Update is called once per frame
-    void Update ()
-    { 
-        // TODO: optimize. Only edge-trigger this.
+    void Update()
+    {
         if ( GameState.instance.is_red_alert )
         {
             light.color = new Color( 1.0f, 0.0f, 0.0f );
 
-            timer += Time.deltaTime * Time.timeScale;
+            timer += Time.deltaTime * Time.timeScale * Mathf.PI * 2.0f / period;
             if ( timer > Mathf.PI * 2.0f )
             {
                 timer -= Mathf.PI * 2.0f;
             }
-            light.intensity =  intensity - 0.5f * intensity * 0.5f * ( Mathf.Sin( timer ) + 1.0f );
+            // 100% : minumum brightness % intensity
+            light.intensity = intensity * ( 1.0f - ( 1.0f - minimum_brightness ) * ( 0.5f * ( Mathf.Sin( timer ) + 1.0f ) ) );
         }
         else
         {
