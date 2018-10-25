f2672b799c555d8ee5a423a164c5d2282e8fefe1
GabeV <gviolette0@gmail.com>
2018-10-25 15:38:54 -0400

    Add noise and sparks on hookshot failure.

--- ProjectStealth/Assets/Scripts/Character/GrapplingHook.cs
+++ ProjectStealth/Assets/Scripts/Character/GrapplingHook.cs
@@ -14,6 +14,7 @@ public class GrapplingHook : MonoBehaviour
     private float distance_from_target = 0.0f;
     private bool  is_on                = false;
     private bool  is_retracting        = false;
+    private bool  did_fail             = false;
     private float angle;
     private Vector2 target;
     //private Player player_script;
@@ -78,24 +79,28 @@ public class GrapplingHook : MonoBehaviour
         CollisionType collision_type = hit.collider.GetComponent<CollisionType>();
         if ( collision_type == null )       { return; }
         if ( collision_type.CanHookshotTo ) { hit_good_surface = true; }
-        else { hit_bad_surface = true; }
+        else                                { hit_bad_surface  = true; }
 
         distance_to_target   = hit.distance;
         distance_from_target = 0.0f;
+        if ( hit_bad_surface ) { target = distance_to_target * direction.normalized + character_position; }
         // TODO: consider snapping to a predefined position on a predefined designed hookshottable object. Makes edge case handling easier.
         // The alternative best way for static geometry would be to raycast from the 4 corners of the player hitbox, and if the center ray works,
         // fire the hookshot, attach, and retract it. Then STOP when you reach the distance where a corner ray hit an obstacle.
         // won't work vs. moving platforms well, so might need to do collision checks every frame (or ignore them?).
-        BoxCollider2D box = hit.collider.GetComponent<BoxCollider2D>();
-        if ( box != null )
+        if ( hit_good_surface )
         {
-            target = box.bounds.center;
-            distance_to_target = Vector2.Distance( character_position, box.bounds.center );
+            BoxCollider2D box = hit.collider.GetComponent<BoxCollider2D>();
+            if ( box != null )
+            {
+                target = box.bounds.center;
+                distance_to_target = Vector2.Distance( character_position, box.bounds.center );
+            }
         }
 
         if      ( hit_good_surface ) { StartGrapple( target ); }
         else if ( hit_enemy )        { GetOverHere(); }
-        else if ( hit_bad_surface )  { NoisilyBounceOffWall(); }
+        else if ( hit_bad_surface )  { NoisilyBounceOffWall( target ); }
     }
 
     private void StartGrapple( Vector2 target )
@@ -120,9 +125,11 @@ public class GrapplingHook : MonoBehaviour
     /// Fires the hookshot, and FAILS.
     /// Can be used to draw attention, though.
     /// </summary>
-    private void NoisilyBounceOffWall()
+    private void NoisilyBounceOffWall( Vector2 target )
     {
-        // make a dink noise and spark
+        // makes a dink noise and sparks
+        did_fail = true;
+        StartGrapple( target );
     }
 
     /// <summary>
@@ -145,6 +152,24 @@ public class GrapplingHook : MonoBehaviour
             {
                 scale += distance_to_target; // don't overshoot
                 is_retracting = true;
+
+                if ( did_fail ) // make dink noise and spark
+                {
+                    GameObject noise_obj = null;
+                    if ( noise_prefab != null ) { noise_obj = GameObject.Instantiate( noise_prefab, hook_instance.transform.position, Quaternion.identity ); }
+                    if ( noise_obj != null )
+                    {
+                        Noise noise = noise_obj.GetComponent<Noise>();
+                        if ( noise != null )
+                        {
+                            noise.radius = 48.0f;
+                            noise.lifetime = 0.5f;
+                        }
+                    }
+                    GameObject.Instantiate( spark_prefab, hook_instance.transform.position, Quaternion.Euler( 0.0f, 0.0f, 180.0f + hook_instance.transform.rotation.eulerAngles.z ) );
+                    AudioSource audio_source = hook_instance.GetComponent<AudioSource>();
+                    if ( audio_source != null ) { audio_source.Play(); }
+                }
             }
             distance_from_target += scale;
 
@@ -156,28 +181,37 @@ public class GrapplingHook : MonoBehaviour
         {
             scale = RETRACT_SPEED * Time.deltaTime * Time.timeScale;
             distance_from_target -= scale;
+
             if ( distance_from_target <= 0.0f )
             {
-                gameObject.transform.position = new Vector3( target.x, target.y, gameObject.transform.position.z );
-                is_on = false;
-                is_retracting = false;
-                char_stats.current_master_state = CharEnums.MasterState.DefaultState;
-                GameObject.Destroy( hook_instance  );
-                GameObject.Destroy( chain_instance );
+                if ( ! did_fail ) { gameObject.transform.position = new Vector3( target.x, target.y, gameObject.transform.position.z ); }
+                ResetState();
             }
             else
             {
-                gameObject.transform.position += scale * new Vector3( Mathf.Cos( angle ), Mathf.Sin( angle ), 0.0f );
-                chain_instance.transform.position += scale * 0.5f * new Vector3( Mathf.Cos( angle ), Mathf.Sin( angle ), 0.0f );
+                if ( ! did_fail )
+                {
+                    gameObject.transform.position     += scale * new Vector3( Mathf.Cos( angle ), Mathf.Sin( angle ), 0.0f );
+                    chain_instance.transform.position += scale * 0.5f * new Vector3( Mathf.Cos( angle ), Mathf.Sin( angle ), 0.0f );
+                }
+                else
+                {
+                    hook_instance.transform.position  -= scale * new Vector3( Mathf.Cos( angle ), Mathf.Sin( angle ), 0.0f );
+                    chain_instance.transform.position -= scale * 0.5f * new Vector3( Mathf.Cos( angle ), Mathf.Sin( angle ), 0.0f );
+                }
                 chain_instance.GetComponent<SpriteRenderer>().size = new Vector2( chain_instance.GetComponent<SpriteRenderer>().size.x - scale * 1.0f, 7.0f );
             }
         }
     }
 
+    /// <summary>
+    /// Forces player out of grapple mode, turns off the hookshot, and cleans up.
+    /// </summary>
     public void ResetState()
     {
         is_on = false;
         is_retracting = false;
+        did_fail = false;
         if ( hook_instance != null )  { GameObject.Destroy( hook_instance ); }
         if ( chain_instance != null ) { GameObject.Destroy( chain_instance ); }
         char_stats.current_master_state = CharEnums.MasterState.DefaultState;
---
+++ ProjectStealth/Assets/Scripts/DestroyMe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// small utilty script
+public class DestroyMe : MonoBehaviour
+{
+    #region vars
+    [SerializeField]
+    private float lifetime = 1.0f;
+    private float timer = 0.0f;
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        timer = 0.0f;
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        timer += Time.deltaTime * Time.timeScale;
+        if ( timer >= lifetime ) { GameObject.Destroy( this.gameObject ); }
+    }
+}
