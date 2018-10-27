1310b94676163e3982252dbadd47685441b36a92
GabeV <gviolette0@gmail.com>
2018-10-27 13:11:46 -0400

    Create title screen, basic UI.

---
+++ ProjectStealth/Assets/Scripts/Menu/ExitGameOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGameOnClick : MonoBehaviour
+{
+    /// <summary>
+    /// Quits the game when the player pushes the quit button.
+    /// </summary>
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Exit();
+        #endif
+    }
+}
