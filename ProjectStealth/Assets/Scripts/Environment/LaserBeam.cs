e29b6f3422fdc9be2a0830217f70e72ab448f57b
GabeV <gviolette0@gmail.com>
2018-10-29 14:12:23 -0400

    Improve laser graphics. Add buzzsaws. Change player character width to 24 pixels.

---
+++ ProjectStealth/Assets/Scripts/Environment/LaserBeam.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Fires a laser from a point to either max range, or a wall.
+public class LaserBeam : MonoBehaviour
+{
+
+    #region vars
+    public bool is_on = true;
+
+    private Transform start_light;
+    private Transform end_light;
+    private ParticleSystem particles;
+    private const float MAX_RANGE = 500.0f;
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        start_light = transform.Find( "Start" );
+        end_light   = transform.Find( "End" );
+        particles   = transform.Find( "Particles" ).GetComponent<ParticleSystem>();
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        SpriteRenderer sprite_renderer = GetComponent<SpriteRenderer>();
+
+        // If off
+        if ( ! is_on )
+        {
+            sprite_renderer.size = new Vector2( 0.0f, 0.0f );
+            collider.size = new Vector2( 0.0f, 0.0f );
+            start_light.gameObject.SetActive( false );
+            end_light.gameObject.SetActive( false );
+            return;
+        }
+
+        // Laser should end at the first solid object it touches.
+        float angle = transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
+        Vector2 direction = new Vector2( Mathf.Cos( angle ), Mathf.Sin( angle ) );
+
+        RaycastHit2D hit = Physics2D.Raycast( transform.position, direction, MAX_RANGE, CollisionMasks.geo_mask | CollisionMasks.jump_through_mask | CollisionMasks.object_mask );
+        float range;
+        if ( hit.collider == null )
+        {
+            range = MAX_RANGE;
+        }
+        else
+        {
+            range = hit.distance;
+        }
+
+        // set up collider and graphics (rotation of transform handles rotational stuff)
+        sprite_renderer.size = new Vector2( range, 8.0f );
+        collider.size = new Vector2( range, collider.size.y );
+        collider.offset = new Vector2( range / 2.0f, 0.0f);
+
+        start_light.gameObject.SetActive( true );
+        end_light.gameObject.SetActive( true );
+        end_light.transform.position = new Vector3( transform.position.x + Mathf.Cos( angle ) * range, transform.position.y + Mathf.Sin( angle ) * range, 0.0f );
+        start_light.GetComponent<SpriteRenderer>().color = new Color( 1.0f, 0.8f, 0.8f, Random.Range( 0.5f, 0.75f ) );
+        end_light.GetComponent<SpriteRenderer>().color   = new Color( 1.0f, 0.8f, 0.8f, Random.Range( 0.5f, 0.75f ) );
+
+        // For some reason the particlesystem API is awkward.
+        //ParticleSystem.MainModule main = particles.main;
+        ParticleSystem.ShapeModule shape = particles.shape;
+        shape.radius = range / 2.0f;
+        shape.position = new Vector3( range / 2.0f, 0.0f, 0.0f );
+        ParticleSystem.EmissionModule emission = particles.emission;
+        emission.rateOverTime = range / 24.0f * 160.0f;
+
+    }
+}
